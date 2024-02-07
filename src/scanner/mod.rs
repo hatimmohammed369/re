034cb2e5a5eddf2a1f388f4b8239bfab66034648
Hatim Muhammed <hatimmohammed369@gmail.com>
2024-02-07 14:49:31 +0200

    Generating TokenName::Escaped* tokens

--- src/scanner/mod.rs
+++ src/scanner/mod.rs
@@ -1,7 +1,7 @@
 #[allow(dead_code)]
 pub mod tokens;
 
-use tokens::*;
+use tokens::{Token, TokenName::*};
 
 pub struct Scanner {
     source: Vec<char>,
@@ -15,8 +15,20 @@ impl Scanner {
         Scanner { source, current }
     }
 
+    pub fn advance(&mut self) {
+        self.current += 1;
+    }
+
+    pub fn has_next(&self) -> bool {
+        self.current < self.source.len()
+    }
+
     pub fn peek(&self) -> char {
         *self.source.get(self.current).unwrap_or(&'\0')
+    }
+
+    pub fn next_char(&self) -> char {
+        *self.source.get(self.current + 1).unwrap_or(&'\0')
     }
 }
 
@@ -25,14 +37,61 @@ impl Iterator for Scanner {
 
     fn next(&mut self) -> Option<Token> {
         let peek = self.peek();
-        if peek == '\0' {
+        if !self.has_next() {
+            // We reached end of input
             return None;
         }
-        let next = Some(Token {
-            name: TokenName::Character { value: peek },
+
+        let mut next = Some(Token {
+            name: Character { value: peek },
             position: self.current,
         });
-        self.current += 1;
+        let next_token = next.as_mut().unwrap();
+
+        match peek {
+            '\\' => {
+                let next_char = self.next_char();
+                let mut found_escaped_char = true;
+                match next_char {
+                    '\\' => {
+                        next_token.name = EscapedSlash;
+                    }
+                    '(' => {
+                        next_token.name = EscapedLeftParen;
+                    }
+                    ')' => {
+                        next_token.name = EscapedRightParen;
+                    }
+                    '|' => {
+                        next_token.name = EscapedPipe;
+                    }
+                    '?' => {
+                        next_token.name = EscapedMark;
+                    }
+                    '*' => {
+                        next_token.name = EscapedStar;
+                    }
+                    '+' => {
+                        next_token.name = EscapedPlus;
+                    }
+                    '.' => {
+                        next_token.name = EscapedDot;
+                    }
+                    _ => {
+                        found_escaped_char = false;
+                    }
+                }
+                if found_escaped_char && self.has_next() {
+                    self.advance();
+                }
+            }
+            _ => {
+                // Any other character.
+                // Nothing to be handled because by default
+                // token name is TokenName::Character
+            }
+        }
+        self.advance();
         next
     }
 }
