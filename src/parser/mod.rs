05329c62ce6e479c9f69ee5cb887cf59d0a5c46d
Hatim Muhammed <hatimmohammed369@gmail.com>
2024-02-10 07:45:50 +0200

    Signaling end of input
    functions whose names begin with `parse_` now return
    Result<Option<Rc<RefCell<Regexp>>>, String>
    The new `Option` part is used to signal that no expression was parsed
    BUT also no syntax error occurred
    this happens at the end of input or at the end of a compound expression
    (group or union)
    The `String` part still holds syntax error message

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -27,35 +27,49 @@ impl Parser {
         // Grab the first token in stream
         self.advance();
         match self.parse_expression() {
-            Ok(regexp) => {
-                // `regexp` is Rc<RefCell<Regexp>>
-                // When done parsing, take the resulting `Regexp`
-                // and copy its data
-                // field `tag` holds data local to expression (namely its type)
-                // and also it's easily clone
-                // fields `parent` and `children` hold counted references
-                // which are easily copied without losing referenced data
-                // those counted references only increase their internal count when cloned
-                let tag = regexp.borrow().tag.clone();
-                // parent is `None` because this `Regexp` is syntax tree root.
-                let parent = None;
-                // Clone Rc's which is merely increasing internal reference count
-                let children = RefCell::new(
-                    regexp
-                        .borrow()
-                        .children
-                        .borrow()
-                        .iter()
-                        .map(Rc::clone)
-                        .collect::<Vec<_>>(),
-                );
+            Ok(option_regexp) => {
+                // `option_regexp` has type Option<Rc<RefCell<Regexp>>>
+                match option_regexp {
+                    Some(regexp) => {
+                        // When done parsing, take the resulting `Regexp`
+                        // and clone (shallow copy) its data
+                        // field `tag` holds data local to expression (namely its type)
+                        // and also it's easily clone
+                        // fields `parent` and `children` hold counted references
+                        // which are easily copied without losing referenced data
+                        // those counted references only increase their internal count when cloned
+                        let tag = regexp.borrow().tag.clone();
+                        // parent is `None` because this `Regexp` is syntax tree root.
+                        let parent = None;
+                        // Clone Rc's which is merely increasing internal reference count
+                        let children = RefCell::new(
+                            regexp
+                                .borrow()
+                                .children
+                                .borrow()
+                                .iter()
+                                .map(Rc::clone)
+                                .collect::<Vec<_>>(),
+                        );
 
-                // Successfully parsed entire source string
-                Ok(Regexp {
-                    tag,
-                    parent,
-                    children,
-                })
+                        // Successfully parsed entire source string
+                        Ok(Regexp {
+                            tag,
+                            parent,
+                            children,
+                        })
+                    }
+                    None => {
+                        // Could not parse source string for some unknown reason
+                        // maybe a bug in code
+                        // Because even an empty source string has at least one
+                        // token, namely EmptyString, thus we can parse a Regexp
+                        // with its `tag` field set to ExpressionTag::EmptyExpression
+                        let source = self.scanner.get_source_string();
+                        eprintln!("Could not parse source string `{source}`");
+                        panic!()
+                    }
+                }
             }
             // A syntax error occurred while parsing source string
             Err(error) => Err(error),
@@ -67,7 +81,7 @@ impl Parser {
     // AFTER THE MOST RECENTLY PARSED EXPRESSION
     // This rule will be `coded` soon
     // Regexp => EmptyString | Union
-    fn parse_expression(&mut self) -> Result<Rc<RefCell<Regexp>>, String> {
+    fn parse_expression(&mut self) -> Result<Option<Rc<RefCell<Regexp>>>, String> {
         // Attempt to parse an empty expression
         // or `a union expression as denoted by grammar rule above` -- coming soon
         match &self.current {
@@ -84,47 +98,67 @@ impl Parser {
                         // two or more EmptyString tokens in row
 
                         // Successfully parsed an empty expression
-                        Ok(Rc::new(RefCell::new(Regexp::new(
+                        Ok(Some(Rc::new(RefCell::new(Regexp::new(
                             ExpressionTag::EmptyExpression,
-                        ))))
+                        )))))
                     }
                     // Attempt to parse a group expression
                     TokenName::LeftParen => self.parse_group(),
                     // Placeholder code
-                    _ => panic!(),
+                    _ => Ok(None),
                 }
             }
             _ => {
                 // end of stream, no more tokens to process
-                panic!()
+                Ok(None) // no expression was parsed
             }
         }
     }
 
-    fn parse_group(&mut self) -> Result<Rc<RefCell<Regexp>>, String> {
+    fn parse_group(&mut self) -> Result<Option<Rc<RefCell<Regexp>>>, String> {
         // Move past opening (
         self.advance();
         // parse an arbitrary expression or report error (? operator)
-        let parsed_expression = self.parse_expression()?;
-        // Advance only when current item has name TokenName::RightParent
-        // or report error `Expected )` (? operator)
-        self.consume(TokenName::RightParen, "Expected )")?;
-        // field `current` now points to the fisrt character (or EmptyString token)
-        // after the closing )
+        match self.parse_expression()? {
+            Some(parsed_expression) => {
+                // `parsed_expression` has type Rc<RefCell<Regexp>>
 
-        // Construct parsed grouped expression
-        let group = Regexp::new(ExpressionTag::Group);
-        // let `group` take ownershiped of the expression it encloses
-        group.children.borrow_mut().push(parsed_expression);
-        // convert `group` to appropriate return type
-        let group = Rc::new(RefCell::new(group));
-        // make enclosed expression `parent` field points to `group`
-        group.borrow_mut().children.borrow_mut()[0]
-            .borrow_mut()
-            .parent = Some(Rc::downgrade(&group));
+                // Advance only when current item has name TokenName::RightParent
+                // or report error `Expected )` (? operator)
+                self.consume(TokenName::RightParen, "Expected )")?;
+                // field `current` now points to the fisrt character (or EmptyString token)
+                // after the closing )
 
-        // Successfully parsed a grouped expression
-        Ok(group)
+                // Construct parsed grouped expression
+                let group = Regexp::new(ExpressionTag::Group);
+                // let `group` take ownershiped of the expression it encloses
+                group.children.borrow_mut().push(parsed_expression);
+                // convert `group` to appropriate return type
+                let group = Rc::new(RefCell::new(group));
+                // make enclosed expression `parent` field points to `group`
+                group.borrow_mut().children.borrow_mut()[0]
+                    .borrow_mut()
+                    .parent = Some(Rc::downgrade(&group));
+
+                // Successfully parsed a grouped expression
+                Ok(Some(group))
+            }
+            None => {
+                // Syntax error: Expected expression after (
+                let error = "Expected expression after (";
+                let error_position = self.current.as_ref().unwrap().position;
+                let mut error_indicator = String::with_capacity(error_position);
+                while error_indicator.len() < error_position {
+                    error_indicator.push(' ');
+                }
+                error_indicator.push('^');
+                let source = self.scanner.get_source_string();
+                Err(format!(
+                    "Syntax error in position {error_position}: {error}\n\
+                    {source}\n{error_indicator}"
+                ))
+            }
+        }
     }
 
     // Read next token in stream
