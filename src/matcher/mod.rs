10c12c75a8f8f8140455a7c61a032ad3ae50b53c
hatimmohammed369 <hatimmohammed369@gmail.com>
2024-02-27 16:43:06 +0200

    Useful match checks

--- src/matcher/mod.rs
+++ src/matcher/mod.rs
@@ -801,3 +801,30 @@ impl Iterator for Matcher {
         match_attempt
     }
 }
+
+// Useful methods
+impl Matcher {
+    // Does some range within the target matches pattern?
+    pub fn is_matching(&mut self) -> bool {
+        match self.next() {
+            None => {
+                // May be Matcher consumed itself
+                // Retry from the very start
+                self.reset(0);
+                self.next().is_some()
+            }
+            Some(_) => true,
+        }
+    }
+
+    // Return true if the whole target fully matches pattern
+    // In other words, there is exactly one match starting from index 0
+    // ending at index N where N is target length
+    pub fn fullmatch(&mut self) -> bool {
+        self.reset(0);
+        match self.next() {
+            Some(m) => m.start == 0 && m.end == self.target.len(),
+            None => false,
+        }
+    }
+}
