e66803e71206335a7362c2f3efbe32baa37901c6
hatimmohammed369 <hatimmohammed369@gmail.com>
2024-03-10 11:57:27 +0200

    Moving from std::rc::{Rc, RefCell} to std::sync Part#1

--- src/parser/syntax_tree.rs
+++ src/parser/syntax_tree.rs
@@ -1,9 +1,8 @@
 // Syntax tree structs (Tokens structures)
 
-use std::cell::RefCell;
 use std::collections::LinkedList;
 use std::fmt::Display;
-use std::rc::{Rc, Weak};
+use std::sync::{Arc, RwLock, Weak};
 
 #[derive(Debug, Clone, Copy)]
 pub enum Quantifier {
@@ -24,7 +23,8 @@ impl Display for Quantifier {
         write!(f, "{string_value}")
     }
 }
-// Expression type
+
+// Expression types
 #[derive(Debug, Clone, Copy)]
 pub enum ExpressionType {
     // Empty string expression
@@ -64,7 +64,7 @@ pub struct ParsedRegexp {
     pub expression_type: ExpressionType,
 
     // Pattern of this (sub)expression
-    pub pattern: String,
+    pub pattern: Arc<str>,
 
     // -- Parent expression of this object
     // * We use a Weak reference to avoid reference cycles
@@ -72,34 +72,31 @@ pub struct ParsedRegexp {
     // thus reference count of either would never be zero
     // and hence they will never be droped
     // * Field `parent` is Option because syntax tree root has no parent
-    // * We use a RefCell to allow interior mutability in case
+    // * We use a RwLock to allow interior mutability in case
     // `parent` needed to modify is data
-    pub parent: Option<Weak<RefCell<ParsedRegexp>>>,
+    pub parent: Option<Weak<RwLock<ParsedRegexp>>>,
 
     // -- Children expressions of this object
-    // * We use RefCell to allow interior mutability
+    // * We use RwLock to allow interior mutability
     // in case we needed to modify Vec `children`
     // or a particular child needs to be modified
-    // * We use Rc because Expressions are always shared
+    // * We use Arc because Expressions are always shared
     // there is no object which owns an `ParsedRegexp`
-    // * We use RefCell<ParsedRegexp> to allow interior mutability
+    // * We use RwLock<ParsedRegexp> to allow interior mutability
     // in case an `ParsedRegexp` needs to be modified
-    pub children: RefCell<Vec<Rc<RefCell<ParsedRegexp>>>>,
+    pub children: RwLock<Vec<Arc<RwLock<ParsedRegexp>>>>,
 }
 
 // Replace `Default` trait with a constructor which at least initializes
 // the object tag field rather than using `ExpressionTag::EmptyExpression`
 // as a default tag, and still gives an `initialized` object
 impl ParsedRegexp {
-    pub fn new(tag: ExpressionType) -> Self {
-        let pattern = String::new();
-        let parent = None;
-        let children = RefCell::new(vec![]);
+    pub fn new(expr_type: ExpressionType) -> Self {
         ParsedRegexp {
-            expression_type: tag,
-            pattern,
-            parent,
-            children,
+            expression_type: expr_type,
+            pattern: Arc::from(""),
+            parent: None,
+            children: RwLock::new(vec![]),
         }
     }
 
@@ -111,16 +108,16 @@ impl ParsedRegexp {
 
         if let Some(parent) = &self.parent {
             let parent = parent.upgrade().unwrap();
-            let parent = &RefCell::borrow(&parent).pattern;
+            let parent = &parent.read().unwrap().pattern;
             debug.push_str(&format!("{indent}parent : {},\n", parent));
         }
 
-        let children = self.children.borrow();
+        let children = self.children.read().unwrap();
         debug.push_str(&format!("{indent}children = {{"));
         if !children.is_empty() {
             debug.push('\n');
             for child in children.iter() {
-                let child = &RefCell::borrow(child).pattern;
+                let child = &child.read().unwrap().pattern;
                 debug.push_str(&format!("{indent}{indent}{child},\n"));
             }
         }
@@ -130,7 +127,7 @@ impl ParsedRegexp {
         debug
     }
 
-    pub fn deep_copy(&self) -> ParsedRegexp {
+    pub fn deep_copy(&self) -> Arc<RwLock<ParsedRegexp>> {
         // Copy expressions one level at a time
         // Each a time iterate through children in order from first to last,
         // copy the child itself then store its children for next iteration
@@ -139,68 +136,63 @@ impl ParsedRegexp {
         // repeat until all children in source ParsedRegexp have been copied
         // in other words all levels copied, we copied last level
         // which its expressions have no children
-        let mut source_children = LinkedList::from([Rc::new(RefCell::new(self.clone()))]);
-        let deep_copy = Rc::new(RefCell::new(ParsedRegexp {
+        let mut source_children = LinkedList::from([Arc::new(RwLock::new(Self::clone(self)))]);
+        let deep_copy = Arc::new(RwLock::new(ParsedRegexp {
             expression_type: self.expression_type,
-            pattern: self.pattern.clone(),
+            pattern: Arc::from(self.pattern.as_ref()),
             parent: None,
-            children: RefCell::new(vec![]),
+            children: RwLock::new(vec![]),
         }));
-        let mut dest_children = LinkedList::from([Rc::clone(&deep_copy)]);
+        let mut dest_children = LinkedList::from([Arc::clone(&deep_copy)]);
 
         while !source_children.is_empty() {
             let source_level_end = source_children.len();
             for _ in 1..=source_level_end {
                 let source_child = source_children.pop_front().unwrap();
-                let source_child = RefCell::borrow(&source_child);
+                let source_child = source_child.read().unwrap();
 
-                let source_child_offspring = source_child.children.borrow();
+                let source_child_offspring = source_child.children.read().unwrap();
                 let source_child_offspring = source_child_offspring.iter().map(|kid| {
-                    source_children.push_back(Rc::clone(kid));
-                    RefCell::borrow(kid)
+                    source_children.push_back(Arc::clone(kid));
+                    kid.read().unwrap()
                 });
 
                 let dest_child = dest_children.pop_front().unwrap();
-                let dest_child_offspring = RefCell::borrow_mut(&dest_child);
-                let dest_child_offspring = &mut dest_child_offspring.children.borrow_mut();
+                let dest_child_offspring = dest_child.write().unwrap();
+                let mut dest_child_offspring = dest_child_offspring.children.write().unwrap();
 
                 for src_kid in source_child_offspring {
-                    let new_dest_child = {
-                        let tag = src_kid.expression_type;
-                        let parent = Some(Rc::downgrade(&dest_child));
-                        let pattern = src_kid.pattern.clone();
-                        let children = RefCell::new(vec![]);
+                    let new_dest_child = Arc::new(RwLock::new(ParsedRegexp {
+                        expression_type: src_kid.expression_type,
+                        parent: Some(Arc::downgrade(&dest_child)),
+                        pattern: Arc::from(src_kid.pattern.as_ref()),
+                        children: RwLock::new(vec![]),
+                    }));
 
-                        Rc::new(RefCell::new(ParsedRegexp {
-                            expression_type: tag,
-                            parent,
-                            pattern,
-                            children,
-                        }))
-                    };
-
-                    dest_children.push_back(Rc::clone(&new_dest_child));
+                    dest_children.push_back(Arc::clone(&new_dest_child));
                     dest_child_offspring.push(new_dest_child);
                 }
             }
         }
 
-        let deep_copy = RefCell::borrow(&deep_copy).clone();
         deep_copy
     }
 }
 
 impl Clone for ParsedRegexp {
     fn clone(&self) -> Self {
-        let tag = self.expression_type;
-        let pattern = self.pattern.clone();
-        let parent = self.parent.as_ref().map(Weak::clone);
-        let children = RefCell::new(self.children.borrow().iter().map(Rc::clone).collect());
         ParsedRegexp {
-            expression_type: tag,
-            pattern,
-            parent,
-            children,
+            expression_type: self.expression_type,
+            pattern: Arc::from(self.pattern.as_ref()),
+            parent: self.parent.as_ref().map(Weak::clone),
+            children: RwLock::new(
+                self.children
+                    .read()
+                    .unwrap()
+                    .iter()
+                    .map(Arc::clone)
+                    .collect(),
+            ),
         }
     }
 }
